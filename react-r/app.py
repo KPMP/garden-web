a741448f2fa49684a85c3b0a155e5fda8e121c21
Zach Wright <zwright@umich.edu>
2020-10-30 15:14:42 -0400

    KPMP-2225: Code cleanup

--- react-r/app.py
+++ react-r/app.py
@@ -1,11 +1,13 @@
 import os, sys, time
 from flask import Flask, redirect, send_file
 import pyRserve
+import pprint
 
 def get_newest_dir(top_dir):
     return max([os.path.join(top_dir,d) for d in os.listdir(top_dir)], key=os.path.getmtime)
 
 app = Flask(__name__)
+time.sleep(20)
 rconn = pyRserve.connect(host='rserve', port=6311)
 connection_dir = get_newest_dir("/r-output")
 rconn.eval('source(file = "../../etc/load_data.R")')
@@ -22,17 +24,16 @@ def test_plot_jpg():
     rconn.eval('barplot(x)')
     rconn.eval('dev.off()')
     file_path = connection_dir + '/' + file_name
-    return 'File saved here: <a target="_blank" href="http://localhost' + file_path + '">file_path</a>'
+    return 'File saved here: <a target="_blank" href="http://localhost/' + file_path + '">file_path</a>'
     
 @app.route('/test_plot_pdf', methods=['GET'])
 def test_plot_pdf():
     file_name = "barplot.pdf";
     rconn.eval("pdf(file="'+file_name+'", width=11, height=8.5)")
     rconn.eval('x <- c(24, 13, 7, 5, 3, 2)')
-    rconn.eval('barplot(x)')
-    rconn.eval('dev.off()')
+    plot_array = rconn.eval('barplot(x)')
     file_path = connection_dir + '/' + file_name
-    return 'File saved here: <a target="_blank" href="http://localhost' + file_path + '">file_path</a>'
+    return 'File saved here: <a target="_blank" href="http://localhost/' + file_path + '">file_path</a>'
     
 @app.route('/umap', methods=['GET'])
 def umap():
@@ -42,7 +43,7 @@ def umap():
     rconn.eval('DimPlot(s_obj, reduction = "umap", label=TRUE, label.size=6, combine=FALSE)')
     rconn.eval('ggsave(file = "'+file_name+'")')
     file_path = connection_dir + '/' + file_name
-    return 'File saved here: <a target="_blank" href="http://localhost' + file_path + '">file_path</a>'
+    return 'File saved here: <a target="_blank" href="http://localhost/' + file_path + '">file_path</a>'
     
 @app.route('/umap_gene/<gene>', methods=['GET'])
 def umap_gene(gene):
@@ -52,7 +53,7 @@ def umap_gene(gene):
     rconn.eval('FeaturePlot(s_obj, reduction = "umap", features = c(toupper("'+ gene +'")), combine=FALSE)')
     rconn.eval('ggsave(file = "'+file_name+'")')
     file_path = connection_dir + '/' + file_name
-    return 'File saved here: <a target="_blank" href="http://localhost' + file_path + '">file_path</a>'
+    return 'File saved here: <a target="_blank" href="http://localhost/' + file_path + '">file_path</a>'
     
     
     
